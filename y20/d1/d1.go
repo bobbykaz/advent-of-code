74c61e283a7bb4b23b0db15b1c0b1299a39032b1
bobby kaz <robert.kazimiroff@gmail.com>
2020-12-02 21:42:49 -0800

    Day 1 Part 1 more efficient; o(n)

--- y20/d1/d1.go
+++ y20/d1/d1.go
@@ -2,6 +2,7 @@ package d1
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/bobbykaz/advent-of-code/utilities"
 )
@@ -9,17 +10,44 @@ import (
 func Part1() int {
 	input := utilities.ReadFileIntoLines("input/y20d1.txt")
 	numbers := utilities.StringsToInts(input)
+	fmt.Println(bruteForceDuo(numbers))
+	fmt.Println(scanDuo(numbers))
+	return -1
+}
+
+func scanDuo(numbers []int) int {
+	sort.Ints(numbers)
+	first, last := 0, len(numbers)-1
+	for first < last {
+		sum := numbers[first] + numbers[last]
+		if sum == 2020 {
+			fmt.Printf("Scan: %d + %d = 2020; multiplied: %d\n", numbers[first], numbers[last], (numbers[first] * numbers[last]))
+			return (numbers[first] * numbers[last])
+		}
+
+		if sum < 2020 {
+			first++
+		} else {
+			last--
+		}
+	}
+	fmt.Println("Scan failed")
+	return -1
+}
+
+func bruteForceDuo(numbers []int) int {
 	for i := 0; i < len(numbers); i++ {
 		for j := 0; j < len(numbers); j++ {
 			if i != j {
 				if (numbers[i] + numbers[j]) == 2020 {
-					fmt.Printf("%d + %d = 2020; multiplied: %d\n", numbers[i], numbers[j], (numbers[i] * numbers[j]))
+					fmt.Printf("Brute force: %d + %d = 2020; multiplied: %d\n", numbers[i], numbers[j], (numbers[i] * numbers[j]))
 
 					return (numbers[i] * numbers[j])
 				}
 			}
 		}
 	}
+	fmt.Println("brute force failed...how?")
 	return -1
 }
 
