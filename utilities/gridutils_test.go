04ee7aad603ef89436a9d1dd43301f9d55055c7b
bobby kaz <robert.kazimiroff@gmail.com>
2020-12-21 23:22:36 -0800

    Grid tests

--- utilities/gridutils_test.go
+++ utilities/gridutils_test.go
@@ -37,3 +37,99 @@ func Test_Grid_Adjacent_UR_Edge(t *testing.T) {
 		t.Fatalf("failed, %v, %v", expected, actual)
 	}
 }
+
+func Test_FlipHorizontal_Odd(t *testing.T) {
+	var input = []string{"123", "456", "789"}
+	g := StringsToGrid(input)
+	g.FlipGridHorizontal()
+
+	var ai = []string{"321", "654", "987"}
+	ag := StringsToGrid(ai)
+
+	if !(g.Equals(&ag, false)) {
+		t.Fatalf("failed, %v, %v", ag, g)
+	}
+}
+
+func Test_FlipHorizontal_Even(t *testing.T) {
+	var input = []string{"12", "34"}
+	g := StringsToGrid(input)
+	g.FlipGridHorizontal()
+
+	var ai = []string{"21", "43"}
+	ag := StringsToGrid(ai)
+
+	if !(g.Equals(&ag, false)) {
+		t.Fatalf("failed, %v, %v", ag, g)
+	}
+}
+
+func Test_FlipVertical(t *testing.T) {
+	var input = []string{"123", "456", "789"}
+	g := StringsToGrid(input)
+	g.FlipGridVertical()
+
+	var ai = []string{"789", "456", "123"}
+	ag := StringsToGrid(ai)
+
+	if !(g.Equals(&ag, false)) {
+		t.Fatalf("failed, %v, %v", ag, g)
+	}
+}
+
+func Test_FlipVertical_Even(t *testing.T) {
+	var input = []string{"12", "34"}
+	g := StringsToGrid(input)
+	g.FlipGridVertical()
+
+	var ai = []string{"34", "12"}
+	ag := StringsToGrid(ai)
+
+	if !(g.Equals(&ag, false)) {
+		t.Fatalf("failed, %v, %v", ag, g)
+	}
+}
+
+func Test_GetEdge_Up(t *testing.T) {
+	var input = []string{"12", "34"}
+	g := StringsToGrid(input)
+	expected := "12"
+	actual := string(g.GetEdge(GridEdgeUp))
+
+	if !(expected == actual) {
+		t.Fatalf("failed, %v, %v", expected, actual)
+	}
+}
+
+func Test_GetEdge_Down(t *testing.T) {
+	var input = []string{"12", "34"}
+	g := StringsToGrid(input)
+	expected := "34"
+	actual := string(g.GetEdge(GridEdgeDown))
+
+	if !(expected == actual) {
+		t.Fatalf("failed, %v, %v", expected, actual)
+	}
+}
+
+func Test_GetEdge_Left(t *testing.T) {
+	var input = []string{"12", "34"}
+	g := StringsToGrid(input)
+	expected := "13"
+	actual := string(g.GetEdge(GridEdgeLeft))
+
+	if !(expected == actual) {
+		t.Fatalf("failed, %v, %v", expected, actual)
+	}
+}
+
+func Test_GetEdge_Right(t *testing.T) {
+	var input = []string{"12", "34"}
+	g := StringsToGrid(input)
+	expected := "24"
+	actual := string(g.GetEdge(GridEdgeRight))
+
+	if !(expected == actual) {
+		t.Fatalf("failed, %v, %v", expected, actual)
+	}
+}
