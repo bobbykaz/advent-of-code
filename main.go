ff6896e3b5e42f4ef4261df202eb6539ffad6b17
bobby kaz <robert.kazimiroff@gmail.com>
2020-12-24 22:55:02 -0800

    Advent of Code 2020 complete.

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/bobbykaz/advent-of-code/y20/d24"
+	"github.com/bobbykaz/advent-of-code/y20/d25"
 )
 
 func main() {
-	output1 := d24.Run()
+	output1 := d25.Run()
 	fmt.Println("Output1: ", output1)
 }
--- y20/d25/d25.go
+++ y20/d25/d25.go
@@ -2,17 +2,49 @@ package d25
 
 import (
 	"fmt"
-
-	"github.com/bobbykaz/advent-of-code/utilities"
 )
-
-var inputFile = "input/y20/d25.txt"
 
 var Print = true
 
 func Run() int {
-	input := utilities.ReadFileIntoLines(inputFile)
-	log("%d\n", len(input))
+	//doorId := 17807724
+	//cardId := 5764801
+	doorId := 12090988
+	cardId := 240583
+
+	sn := 7
+	value := 1
+	doorloop := 0
+	for value != doorId {
+		value *= sn
+		value = value % 20201227
+		doorloop++
+	}
+
+	log("Door loopsize: %d\n", doorloop)
+
+	cardloop := 0
+	value = 1
+	for value != cardId {
+		value *= sn
+		value = value % 20201227
+		cardloop++
+	}
+	log("card loopsize: %d\n", cardloop)
+
+	value = 1
+	for i := 0; i < cardloop; i++ {
+		value *= doorId
+		value = value % 20201227
+	}
+	log("Encryption Key path 1: %d\n", value)
+	value = 1
+	for i := 0; i < doorloop; i++ {
+		value *= cardId
+		value = value % 20201227
+	}
+
+	log("Encryption Key path 2: %d\n", value)
 	return -1
 }
 
