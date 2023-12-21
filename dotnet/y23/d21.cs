168e8608b606484eb0ed86424c4deeb1b7676413
Bobby Kazimiroff <robert.kazimiroff@gmail.com>
2023-12-20 23:25:40 -0800

    Part 2 after some help from reddit

--- dotnet/y23/d21.cs
+++ dotnet/y23/d21.cs
@@ -65,58 +65,50 @@ namespace y23 {
         public override string P2()
         {
             var lines = InputAsLines();
-            var grid = Utilties.RectangularCharGridFromLines(lines);
-            var(rs,cs) = (0,0);
-            grid.ForEachRowCol((r,c,v) =>{
-                if(v == 'S') {
-                    rs = r;
-                    cs = c;
-                }
-            });
-
-            var prevCells = new List<MvCell>
-            {
-                new MvCell(new Cell<char>(rs, cs, 'S'))
-            };
-            prevCells[0].planes.Add((0,0));
-            grid.G[rs][cs] = '.';
-            int NumSteps = 26501365;
-            for(int i = 1; i <= NumSteps; i++) {
-                var nextCells = new Dictionary<string, MvCell>();
-                foreach(var mvc in prevCells) {
-                    var nn = grid.CardinalNeighborsWrapped(mvc.Cell.R, mvc.Cell.C).Where(c => c.Item1.V == '.').ToList();
-                    foreach(var (nc, d, wrapped) in nn) {
-                        string key = $"{nc.R}-{nc.C}";
-                        if(!wrapped){
-                            if(nextCells.ContainsKey(key))
-                                nextCells[key].planes.UnionWith(mvc.planes);
-                            else 
-                                nextCells[key] = new MvCell(nc, mvc.planes);
-                        } else {
-                            var curPlanes = new HashSet<(int,int)>(mvc.planes);
-                            var nextPlanes = curPlanes.Select(p => d switch {
-                                Dir.N => (p.Item1-1,p.Item2),
-                                Dir.S => (p.Item1+1,p.Item2),
-                                Dir.E => (p.Item1,p.Item2+1),
-                                Dir.W => (p.Item1,p.Item2-1),
-                                _ => throw new Exception()
-                            }).ToHashSet();
-                            curPlanes.UnionWith(nextPlanes);
-                            if(nextCells.ContainsKey(key))
-                                nextCells[key].planes.UnionWith(curPlanes);
-                            else 
-                                nextCells[key] = new MvCell(nc, curPlanes);
-                        }
-                    }
-                }
-                prevCells = nextCells.Values.ToList();
-                if( i % 100 == 0) {
-                    PrintLn($"{i}: {_ElapsedMillis}");
+            var baseGrid = Utilties.RectangularCharGridFromLines(lines);
+            var grid = new Grid<char>(baseGrid.Width*5, baseGrid.Height * 5, '.');
+            for(int i = 0; i < 5; i++) {
+                for(int j = 0; j < 5; j++) {
+                    baseGrid.ForEachColRow((r,c,v) => {
+                        char n = v == 'S' ? '.' : v;
+                        grid.G[baseGrid.Width*i + r][baseGrid.Width*j + c] = n;
+                    });
                 }
             }
-            var total = prevCells.Sum( c => (long)c.planes.Count);
-            return $"{total}";
+            var(rs,cs) = (baseGrid.Width*2 + 65,baseGrid.Width*2 + 65);
 
+           var stepMap = new Dictionary<int, List<Cell<char>>>();
+            stepMap[0] = new List<Cell<char>>
+            {
+                new Cell<char>(rs, cs, 'S')
+            };
+            grid.G[rs][cs] = '.';
+            //var vm = new VisitedMap();
+            //vm.Visit(rs,cs);
+            int NumSteps = 350;
+            for(int i = 1; i <= NumSteps; i++) {
+                stepMap[i] = new List<Cell<char>>();
+                var vm = new VisitedMap();
+                foreach(var cell in stepMap[i-1]) {
+                    var nn = grid.CardinalNeighbors(cell.R, cell.C).Where(c => c.V == '.' && !vm.WasVisited(c.R,c.C)).ToList();
+                    foreach(var nc in nn) {
+                        vm.Visit(nc.R,nc.C);
+                        stepMap[i].Add(nc);
+                    }
+                }
+                PrintLn($"{i}: {stepMap[i].Count}");
+            }
+            foreach(var c in stepMap[NumSteps]) {
+                grid.G[c.R][c.C] = 'O';
+            }
+            //grid.Print();
+            return $"Full grid:{stepMap[65].Count}; Full grid plus 1 grid length: {stepMap[65+131].Count};full grid plus 2 grid length {stepMap[65+262].Count}";
+            // fit answer to a quadratic formula
+            // each time you move a grid length (131), you number of grids is squared by the side of the grid; 1, 4, 9, etc
+            // 65 - number in the first grid
+            // 65 + 131 - gets you to the center of any of the '2nd' grids, then explores that whole grid, etc
+            // plug in the proper value for (26501365 - 65) / 131 = 202300
+            // is this right?
         }
 
     }
