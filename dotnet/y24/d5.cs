cb4a4c64297a3f1af695f6ef38552f8ae00f2326
Bobby Kazimiroff <robert.kazimiroff@gmail.com>
2024-12-04 23:28:49 -0800

    2024 day 5 part 2

--- dotnet/y24/d5.cs
+++ dotnet/y24/d5.cs
@@ -31,8 +31,8 @@ namespace y24 {
 
                 var before = rules[pts[0]];
                 var after = rules[pts[1]];
-                before.Before.Add(after.Val);
-                after.After.Add(before.Val);
+                before.MustBeBefore.Add(after.Val);
+                after.MustBeAfter.Add(before.Val);
             }
 
             PrintLn($"Rules: {rules.Count}");
@@ -55,18 +55,18 @@ namespace y24 {
         private bool checkUpdate(Dictionary<int, Rule> rules, List<int> nums) {
             foreach( var (n,i) in nums.ForEachIndex()) {
                 var rule = rules[n] ?? throw new Exception();
-                PrintLn($"Checking {rule.ToString()}\n");
+                PrintLn($"Checking {rule}\n");
                 var isAfterNums = nums.Take(i-1);
                 var isBeforeNums = nums.Skip(i);
                 foreach(var nn in isAfterNums){
-                    if(rule.Before.Contains(nn)){
+                    if(rule.MustBeBefore.Contains(nn)){
                         PrintLn($"  Failed on {nn} - in After");
                         return false;
                     }
                 }
 
                 foreach(var nn in isBeforeNums){
-                    if(rule.After.Contains(nn)){
+                    if(rule.MustBeAfter.Contains(nn)){
                         PrintLn($"  Failed on {nn} - in Before");
                         return false;
                     }
@@ -82,18 +82,86 @@ namespace y24 {
 
         public class Rule {
             public int Val;
-            public HashSet<int> Before;
-            public HashSet<int> After;
+            public HashSet<int> MustBeBefore;
+            public HashSet<int> MustBeAfter;
             public Rule(int number){
                 Val = number;
-                Before = new HashSet<int>();
-                After = new HashSet<int>();
+                MustBeBefore = new HashSet<int>();
+                MustBeAfter = new HashSet<int>();
             }
 
             public override string ToString()
             {
-                return $"{Val} \n Before: {string.Join(",", Before.ToArray())}\n After: {string.Join(",", After.ToArray())}";
+                return $"{Val} \n Before: {string.Join(",", MustBeBefore.ToArray())}\n After: {string.Join(",", MustBeAfter.ToArray())}";
             }
+        }
+
+        private class RulesComparer : IComparer<int>
+        {
+            private Dictionary<int, Rule> Rules;
+            public RulesComparer(Dictionary<int, Rule> rules) {
+                Rules = rules;
+            }
+            public int Compare(int x, int y)
+            {
+                var xr = Rules[x];
+                if(xr.MustBeBefore.Contains(y)) {
+                    return -1;
+                }
+
+                if (xr.MustBeAfter.Contains(y)) {
+                    return 1;
+                }
+
+                return 0;
+            }
+
+        }
+
+        private int sortNums(Dictionary<int, Rule> rules, List<int> nums) {
+            var comp = new RulesComparer(rules);
+            nums.Sort(comp);
+            return middlePage(nums);
+        }
+
+        private int reorderNums(Dictionary<int, Rule> rules, List<int> nums) {
+            var chosen = new List<int>();
+            PrintLn($"Checking: {string.Join(",", nums)}");
+            while(nums.Any()) {
+                var anyFound = false;
+                foreach(var (n,i) in nums.ForEachIndex()){
+                    var remaining = new List<int>(nums).Where(s => s != n).ToList();
+                    PrintLn($"  {n} - Remaining: {string.Join(",", remaining)}");
+                    //n must be AFTER everything in newList and BEFORE everything in remaining
+                    var rule = rules[n] ?? throw new Exception();
+
+                    var anyRemainingShouldBeBefore = remaining.Any(x => rule.MustBeBefore.Contains(x));
+                    var anyAlreadyChosenShouldBeAfter = chosen.Any(x => rule.MustBeAfter.Contains(x));
+                    if(!anyAlreadyChosenShouldBeAfter && !anyRemainingShouldBeBefore) {
+                        PrintLn($"  adding {n} to list");
+                        nums.RemoveAt(i);
+                        chosen.Add(n);
+                        PrintLn($" now {string.Join(",", chosen)}");
+                        anyFound = true;
+                        break;
+                    }
+                }
+                if(!anyFound){
+                    PrintLn("  Didnt find anything");
+                    throw new Exception();
+                }
+            }
+
+            chosen.Reverse();
+
+            PrintLn($"Checking final pass of {string.Join(",", chosen)}");
+            _DebugPrinting = false;
+            if(!checkUpdate(rules, chosen)) {
+                _DebugPrinting = true;
+                PrintLn($"Something didnt go right!");
+            }
+
+            return middlePage(chosen);
         }
 
         public override string P2()
@@ -118,8 +186,8 @@ namespace y24 {
 
                 var before = rules[pts[0]];
                 var after = rules[pts[1]];
-                before.Before.Add(after.Val);
-                after.After.Add(before.Val);
+                before.MustBeBefore.Add(after.Val);
+                after.MustBeAfter.Add(before.Val);
             }
 
             PrintLn($"Rules: {rules.Count}");
@@ -131,9 +199,11 @@ namespace y24 {
                 PrintLn($"\n===\nEval: {line}");
                 var n = Utilties.StringToNums<int>(line, ",");
                 if(!checkUpdate(rules, n)){
-                    var m = middlePage(n);
+                    _DebugPrinting = true;
+                    var m = reorderNums(rules, n);
                     total += m;
                     PrintLn($"update {line} valid; adding {m}; total {total}");
+                    _DebugPrinting = false;
                 }
             }
             return $"{total}";
