77d2e47732c9febf76c2cc9ec7ee88df63d54790
Bobby Kazimiroff <robert.kazimiroff@gmail.com>
2024-12-11 23:41:01 -0800

    Day 11 part 2

--- dotnet/y24/d11.cs
+++ dotnet/y24/d11.cs
@@ -47,18 +47,50 @@ namespace y24 {
         {
             var lines = InputAsLines();
             var nums = Utilties.StringToNums<long>(lines[0], " ");
+            var initInput = new List<long>(nums);
+            Dictionary<long, List<long>> evalMap = [];
+            var currentSet = new HashSet<long>();
+            foreach(var l in nums) {currentSet.Add(l);}
+            // populate all items in the set - 75 rounds
             for(int i = 0; i < 75; i++) {
-                PrintLn($"Round {i}");
-                var newNums = new List<long>();
-                for(int n = 0; n < nums.Count; n++) {
-                    var pts = eval(nums[n]);
-                    newNums.AddRange(pts);
+                var newSet = new HashSet<long>();
+                foreach(var n in currentSet) {
+                    if(!evalMap.ContainsKey(n)) {
+                        var nextNums = eval(n);
+                        evalMap[n] = nextNums;
+                        foreach(var nn in nextNums) {newSet.Add(nn);};
+                    }
                 }
-                nums = newNums;
+                currentSet = newSet;
             }
-            var total = nums.Count;
+            PrintLn($"Done prefilling values: {evalMap.Keys.Count} keys");
+
+            //memoize (val, round) stone-count = memos[round-1][evalMap[val]]
+            var memos = new Dictionary<string, long>();
+            for(int round = 0; round <= 75; round++) {
+                foreach(var val in evalMap.Keys) {
+                    if(round == 0) {
+                        memos[StoneKey(val, round)] = 1;
+                    } else {
+                        var evs = evalMap[val];
+                        long subtotal = 0;
+                        foreach(var ev in evs) {
+                            subtotal += memos[StoneKey(ev, round-1)];
+                        }
+                        memos[StoneKey(val, round)] = subtotal;
+                    }
+                }
+            }
            
+            long total = 0;
+            foreach(var n in initInput) {
+                total += memos[StoneKey(n, 75)];
+            }
             return $"{total}";
+        }
+        
+        private string StoneKey(long val, int round) {
+            return $"{val}-{round}";
         }
 
     }
