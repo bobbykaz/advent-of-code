d7822bd90b100d8dd5f227d59d179ef111e204f5
Bobby Kazimiroff <robert.kazimiroff@gmail.com>
2024-12-14 08:31:40 -0800

    Day 13 Part 2

--- dotnet/y24/d13.cs
+++ dotnet/y24/d13.cs
@@ -54,10 +54,14 @@ namespace y24 {
             public Coord BtnA;
             public Coord BtnB;
             public Coord Target;
-            public Game(Coord a, Coord b, Coord t) {
+            public Game(Coord a, Coord b, Coord t, bool isP2 = false) {
                 BtnA = a;
                 BtnB = b;
                 Target = t;
+                if(isP2) {
+                    Target.x += 10000000000000L;
+                    Target.y += 10000000000000L;
+                }
             }
 
             public long? Solve() {
@@ -97,12 +101,61 @@ namespace y24 {
                 }
                 return lowestTokens;
             }
+
+            public (bool, long) Math() {
+                //Button A: X+26, Y+66
+                //Button B: X+67, Y+21
+                //Prize: X=10000000012748, Y=10000000012176
+
+                //10000000012748 = APresses*26 + BPresses*67
+                //10000000012176 = Apresses*66 + BPresses*21
+                //| 26   67  |    |Apresses|   = [TX]
+                //| 66   21  |    |BPresses|   = [TY]
+
+                //| a1   b1  |    |x|   = [c1]
+                //| a2   b2  |    |y|   = [c2]
+                var a1 = BtnA.x;
+                var b1 = BtnB.x;
+                var a2 = BtnA.y;
+                var b2 = BtnB.y;
+                var c1 = Target.x;
+                var c2 = Target.y;
+
+                var D = a1*b2 -a2*b1;
+                var Dx = c1*b2 - c2*b1;
+                var Dy = a1*c2 - a2*c1;
+                if (D!= 0 && Dx % D == 0 && Dy % D ==0) {
+                    var X = Dx/D;
+                    var Y = Dy/D;
+                    return (true, (3 * X) + Y);
+                }
+                return (false, 0L);
+
+            }
         }
 
         public override string P2()
         {
             var lines = InputAsLines();
+            var blocks = Utilties.GroupInputAsBlocks(lines);
+            var games = new List<Game>();
             var total = 0L;
+            foreach(var block in blocks) {
+                var ba = new Coord(block[0], false);
+                var bb = new Coord(block[1], false);
+                var t = new Coord(block[2], true);
+                var g = new Game(ba,bb,t, isP2: true);
+                PrintLn($"{ba}, {bb}, : {t}");
+                
+                var (works, tokens) = g.Math();
+                if(works){
+                    PrintLn($"{works} : {tokens}");
+                    total += tokens;
+                }
+                    
+            }
+
+            
            
             return $"{total}";
         }
